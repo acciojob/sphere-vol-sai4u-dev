367179e5e63925b864fdbcc7e06380a641c9af53
Sai Narendra <tannirusainarendra81@gmail.com>
2025-08-10 21:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    event.preventDefault();
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    let radius = parseFloat(document.getElementById('radius').value);
+
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById('volume').value = NaN;
+        return false;
+    }
+
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    document.getElementById('volume').value = volume.toFixed(4);
+
+    return false; 
+}
+
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
